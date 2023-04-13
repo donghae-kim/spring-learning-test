66814e126984239e1ab595515633d53e1c2e25d9
donghae <donghae1999@gmail.com>
2023-04-13 15:36:19 +0900

    feat: Spring core 구현

--- src/main/java/nextstep/helloworld/core/di/StationFieldService.java
+++ src/main/java/nextstep/helloworld/core/di/StationFieldService.java
@@ -1,7 +1,16 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationFieldService {
+    @Autowired
     private StationRepository stationRepository;
+
+    public StationFieldService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
